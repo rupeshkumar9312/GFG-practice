cb98b07d7116675bd240fd4b6bb43c21baeccd27
Rupesh Kumar <rupeshkumar9312@gmail.com>
2020-12-29 21:55:58 +0530

    Pattern of Strings

--- src/com/geeksforgeeks/practice/school/PatternOfStrings.java
+++ src/com/geeksforgeeks/practice/school/PatternOfStrings.java
@@ -5,6 +5,36 @@ import java.util.Scanner;
 
 import static java.lang.System.*;
 
+/*
+*Given a string of length N, print the pattern of strings with first line with the given length of string and keep printing in new line the strings in decreasing order of length till you print the single first character of the given string.
+
+Input: First line of the input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case consist of only one line which contains a string of length N.
+
+Output: Corresponding to each test case, pattern is printed in a new line.
+
+Constraints:
+1 <=T<= 30
+N = 5
+
+Example:
+Input:
+2
+Geeks
+th%ik
+
+Output:
+Geeks
+Geek
+Gee
+Ge
+G
+th%ik
+th%i
+th%
+th
+t
+*  */
+
 public class PatternOfStrings {
 
     public static void main(String[] args) {
